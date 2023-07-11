06601cc2d18727eb0a1409a952ce34845a193ff9
Paulo Salvatore <salvatoregames@gmail.com>
2023-07-11 10:28:22 -0300

    Imports otimizados

--- app/src/main/java/com/oceanbrasil/android_intro_julho_2023/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/android_intro_julho_2023/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.oceanbrasil.android_intro_julho_2023
 
-import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
