689b7a9004285eea4fc386bb1fc80123e0c39ff4
Paulo Salvatore <salvatoregames@gmail.com>
2023-07-11 11:19:01 -0300

    Botão voltar implementado

--- app/src/main/java/com/oceanbrasil/android_intro_julho_2023/ResultadoActivity.kt
+++ app/src/main/java/com/oceanbrasil/android_intro_julho_2023/ResultadoActivity.kt
@@ -2,10 +2,17 @@ package com.oceanbrasil.android_intro_julho_2023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ResultadoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
+
+        val btVoltar = findViewById<Button>(R.id.btVoltar)
+
+        btVoltar.setOnClickListener {
+            finish()
+        }
     }
 }
